99699d70d1c8fd26dee6a452ed8d1249cabc380c
fretuks <frederik@spirgi.com>
2025-11-04 09:41:19 +0100

    Fix to Account Repository

--- src/main/java/ch/bzz/AccountRepository.java
+++ src/main/java/ch/bzz/AccountRepository.java
@@ -3,5 +3,5 @@ package ch.bzz;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 public interface AccountRepository extends JpaRepository<Account, Integer> {
-    List<Account> findByProject(String project);
+    List<Account> findByProject(Project project);
 }
